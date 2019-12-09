893d500ffdf6893f1f19f3479974f90742859090
Miguel angel Fernandez carrillo <migferna@c3r14s6.42madrid.com>
2019-12-09 18:59:56 +0100

    ft_printf basic

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,22 +6,22 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/01 17:47:09 by migferna          #+#    #+#             */
-/*   Updated: 2019/12/05 18:28:29 by migferna         ###   ########.fr       */
+/*   Updated: 2019/12/09 18:58:57 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
+#include "libft.h"
 #include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
 	int count;
-	t_printf data;
+	t_printf *data;
 
 	if (!(data = malloc(sizeof(t_printf))))
-		return (NULL);
+		return (0);
 	count = 0;
-	va_start(data.args, format);
+	va_start(data->args, format);
 	while (*format)
 	{
 		if (*format == '%')
@@ -30,18 +30,17 @@ int	ft_printf(const char *format, ...)
 		}
 		else
 		{
-			//ft_putchar_fd(*format, 1);
-			write(1, &format, 1);
+			ft_putchar_fd(*format, 1);
 			count++;
 		}
 		format++;
 	}
-	va_end(data.args);
+	va_end(data->args);
 	return (count);
 }
 
 int main(void)
 {
-	ft_printf("hola");
+	ft_printf("Hola mundo");
 	return (0);
 }
