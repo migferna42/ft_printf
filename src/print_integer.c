8c3f313f031a17b5b5a85d0f434bc5f1b39e612c
Miguel angel Fernandez carrillo <migferna@c3r14s5.42madrid.com>
2019-12-21 16:37:39 +0100

    40 test passed conversion d

--- src/print_integer.c
+++ src/print_integer.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 16:50:34 by migferna          #+#    #+#             */
-/*   Updated: 2019/12/20 13:27:49 by migferna         ###   ########.fr       */
+/*   Updated: 2019/12/21 16:36:49 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,20 @@
 
 t_printf	*ft_print_integer(t_printf *data)
 {
-	/*if (data->minus_flag == 1)
-		ft_putnbr_fd(number, 1);*/
-	/*int len;
-	int width;
-	
-	if (number < 0)
-	{
-		number *= -1;
-		if (data->width == 0)
-			ft_putchar_fd('-', 1);
-	}
-	len = ft_strlen(ft_itoa(number));
-	width = (data->width + data->precision ) - len;
-	if (data->minus_flag == 1)
-		ft_putnbr_fd(number, 1);
-	while (width-- > 0)
-	{
-		if (data->zero_flag == 1)
-			ft_putchar_fd('0', 1);
-		else
-			ft_putchar_fd(' ', 1);
-	}
-	if (data->minus_flag == 0)
-		ft_putnbr_fd(number, 1);*/
 	int num;
 	int length;
+	int spaces;
 
 	num = va_arg(data->args, int);
 	length = ft_strlen(ft_itoa(num));
+	spaces = length;
+	if ( length <= data->precision && data->precision > -1 )
+		spaces = data->precision;
+	if (data->minus_flag == 0)
+		ft_display(data, ' ', data->width - spaces);
+	ft_display(data, '0', data->precision - data->width);
+	ft_putnbr_fd(num, 1);
+	if (data->minus_flag == 1)
+		ft_display(data, ' ', data->width - spaces);
 	return (data);
 }
