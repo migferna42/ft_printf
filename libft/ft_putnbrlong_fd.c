11bf775df5b380b9340790499fec51d25a477b7e
Miguel angel Fernandez carrillo <migferna@c3r14s3.42madrid.com>
2020-01-07 19:50:52 +0100

    rename function libft

--- libft/ft_putnbrlong_fd.c
+++ libft/ft_putnbrlong_fd.c
@@ -6,13 +6,13 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/07 19:34:39 by migferna          #+#    #+#             */
-/*   Updated: 2020/01/07 19:36:28 by migferna         ###   ########.fr       */
+/*   Updated: 2020/01/07 19:50:17 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbrmax_fd(long int number, int fd)
+void	ft_putnbrlong_fd(long int number, int fd)
 {
 	if (number == -9223372036854775807 - 1)
 		write(1, "-9223372036854775808", 20);
@@ -24,7 +24,7 @@ void	ft_putnbrmax_fd(long int number, int fd)
 			number *= -1;
 		}
 		if (number > 9)
-			ft_putnbrmax_fd(number / 10, fd);
+			ft_putnbrlong_fd(number / 10, fd);
 		ft_putchar_fd((number % 10) + '0', fd);
 	}
 }
